5f39e7ae219afc601f46325472fa5049a9cddf34
chenguangzeng <chenguangzeng@cgz.com>
2024-10-25 18:05:04 +0800

    add example

--- container/set.cpp
+++ container/set.cpp
@@ -1,21 +1,125 @@
 #include <set>
 #include <iostream>
+#include <string>
 
-void BasicSet() {
-  std::set<int> s;
-  s.insert(1);
-  s.insert(2);
-  s.insert(3);
-  s.insert(4);
-  s.insert(5);
+// template < class T,                        // set::key_type/value_type
+//            class Compare = less<T>,        // set::key_compare/value_compare           
+//            class Alloc = allocator<T>      // set::allocator_type           
+// > class set;
 
-  for (auto it = s.begin(); it != s.end(); ++it) {
-    std::cout << *it << " ";
-  }
-  std::cout << std::endl;
+class A {
+public:
+    A() {
+        std::cout << "construct" << std::endl;
+    }
+    A(int a):a(a) {
+        std::cout << "another construct" << std::endl;
+    }
+    ~A() {
+        std::cout << "deconstruct" << std::endl;
+    }
+
+    A(const A &) {
+        std::cout << "copy construct" << std::endl;
+    }
+
+    A& operator=(const A &) {
+        std::cout << "copy assign construct" << std::endl;
+        return *this;
+    }
+
+    A(A&&) {
+        std::cout << "move construct" << std::endl;
+    }
+
+    A& operator=(A&&) {
+        std::cout << "move assign construct" << std::endl;
+        return *this;
+    }
+
+public:
+    int a = 0;
+    double b = 0.0;
+};
+
+void BasicSetMemberTypes() {
+    std::set<int> s;
+    if(std::is_same<std::set<int>::key_type, int>::value) {
+      std::cout << "set<int>::key_type is int" << std::endl;
+    }
+    if(std::is_same<std::set<int>::value_type, int>::value) {
+      std::cout << "set<int>::value_type is int" << std::endl;
+    }
+    if(std::is_same<std::set<int>::key_compare, std::less<int>>::value) {
+      std::cout << "set<int>::key_compare is std::less<int>" << std::endl;
+    }
+    if(std::is_same<std::set<int>::value_compare, std::less<int>>::value) {
+      std::cout << "set<int>::value_compare is std::less<int>" << std::endl;
+    }
+    if(std::is_same<std::set<int>::allocator_type, std::allocator<int>>::value) {
+      std::cout << "set<int>::allocator_type is std::allocator<int>" << std::endl;
+    }
+    if(std::is_same<std::set<int>::reference, int&>::value) {
+      std::cout << "set<int>::reference is int&" << std::endl;
+    }
+    if(std::is_same<std::set<int>::pointer, int*>::value) {
+      std::cout << "set<int>::pointer is int*" << std::endl;
+    }
+    if(std::is_same<std::set<int>::pointer, std::allocator<int>::pointer>::value) {
+      std::cout << "set<int>::pointer is std::allocator<int>::pointer" << std::endl;
+    }
+    if(std::is_same<std::set<int>::iterator, std::iterator<std::bidirectional_iterator_tag, int>>::value) {
+      std::cout << "set<int>::iterator is std::iterator<std::bidirectional_iterator_tag, int>" << std::endl;
+    } else {
+      std::cout << "set<int>::iterator is not std::iterator<std::bidirectional_iterator_tag, int>" << std::endl;
+    }
+}
+
+void BasicConstructor() {
+  // empty
+  std::set<int> s1;
+
+  // initializer list
+  std::set<int> s2 = {1, 2, 3, 4, 5};
+
+  // copy
+  std::set<int> s3(s2);
+
+  // range
+  std::set<int> s4(s2.begin(), s2.end());
+
+  // move
+  std::set<int> s5 = s2;
+  std::set<int> s6 = std::move(s2);
+}
+
+void BasicMember() {
+  std::set<std::string> s = {"1", "2", "3", "4", "5"};
+
+  //1.iterator，看起来是个双向的迭代器
+  std::cout << "s.begin: " << *s.begin() << std::endl;
+  std::cout << "s.end: " << *(--s.end()) << std::endl;
+  std::cout << "s.rbegin: " << *s.rbegin() << std::endl;
+  std::cout << "s.rend: " << *(--s.rend()) << std::endl;
+  std::cout << "s.cbegin: " << *s.cbegin() << std::endl;
+  std::cout << "s.cend: " << *(--s.cend()) << std::endl;
+  std::cout << "s.crbegin: " << *s.crbegin() << std::endl;
+  std::cout << "s.crend: " << *(--s.crend()) << std::endl;
+
+  //2.capacity
+  std::cout << "s.size: " << s.size() << std::endl;
+  std::cout << "s.max_size: " << s.max_size() << std::endl;
+  std::cout << "s.empty: " << s.empty() << std::endl;
+
+  //3.modifiers
 }
 
 int main() {
-  BasicSet();
+  std::cout << "--------------------------------BasicSetMemberTypes--------------------------------" << std::endl;
+  BasicSetMemberTypes();
+  std::cout << "--------------------------------BasicMember--------------------------------" << std::endl;
+  BasicMember();
+  std::cout << "--------------------------------BasicConstructor--------------------------------" << std::endl;
+  BasicConstructor();
   return 0;
 } 
---
+++ type_traits/type_traits.cpp
@@ -0,0 +1,69 @@
+#include <type_traits>
+#include <iostream>
+
+/**
+    在编译期间就能知道值得常量
+ */
+void HelperClass() {
+    // template <class T, T v>
+    // struct integral_constant {
+    // static constexpr T value = v;
+    // typedef T value_type;
+    // typedef integral_constant<T,v> type;
+    // constexpr operator T() const noexcept { return v; }
+    // constexpr T operator()() const noexcept { return v; }
+    // };
+
+    // typedef integral_constant<bool,true> true_type;
+
+    std::true_type t;
+    std::cout << "t.value: " << t.value << std::endl;
+    std::cout << "t(): " << t() << std::endl;
+    std::cout << "t: " << t << std::endl;
+}
+
+void TypeTraits() {
+    // 1.template <class T, class U> struct is_same;
+    std::is_same<int, int>::value_type value = false;
+
+    std::is_same<int, int>::type value2;
+    std::cout << "value2.value: " << value2() << std::endl;
+
+    std::cout << "value: " << std::is_same<int, int>::value << std::endl; //Inherited from integral_constant:
+
+    if(std::is_same<int, int>()) {
+        std::cout << "int and int are the same type" << std::endl;
+    } else {
+        std::cout << "int and int are not the same type" << std::endl;
+    }
+}
+
+// 1. the return type (bool) is only valid if T is an integral type:
+template <class T>
+typename std::enable_if<std::is_integral<T>::value,bool>::type
+is_odd (T i) {return bool(i%2);}
+
+// 2. the second template argument is only valid if T is an integral type:
+template < class T,
+        class = typename std::enable_if<std::is_integral<T>::value>::type>
+bool is_even (T i) {return !bool(i%2);}
+
+void Transform() {
+    // 1.template <bool Cond, class T = void> struct enable_if;
+    // The type T is enabled as member type enable_if::type if Cond is true.Otherwise, enable_if::type is not defined.
+
+    short int i = 1;    // code does not compile if type of i is not integral
+
+    std::cout << std::boolalpha;
+    std::cout << "i is odd: " << is_odd(i) << std::endl;
+    std::cout << "i is even: " << is_even(i) << std::endl;
+}
+
+int main() {
+    std::cout << "--------------------------------HelperClass--------------------------------" << std::endl;
+    HelperClass();
+    std::cout << "--------------------------------TypeTraits--------------------------------" << std::endl;
+    TypeTraits();
+    std::cout << "--------------------------------Transform--------------------------------" << std::endl;
+    Transform();
+}
