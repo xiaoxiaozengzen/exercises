790e2ed853ae3f0f48c121331f4bb9d1f02a52c9
chenguangzeng <chenguangzeng@megvii.com>
2025-01-05 16:50:13 +0800

    feat: add new

--- class/operator.cpp
+++ class/operator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <new>
 
 // 操作符重载范围
 // 1.如果一个重载操作符是类成员，那么只有当与
@@ -102,6 +103,144 @@ class OutExample {
     int a;
 };
 
+/************************************operator new**************************************** */
+// throwing (1)	
+// void* operator new (std::size_t size) throw (std::bad_alloc);
+// nothrow (2)	
+// void* operator new (std::size_t size, const std::nothrow_t& nothrow_value) throw();
+// placement (3)	
+// void* operator new (std::size_t size, void* ptr) throw();
+
+// ordinary (1)	
+// void operator delete (void* ptr) noexcept;
+// nothrow (2)	
+// void operator delete (void* ptr, const std::nothrow_t& nothrow_constant) noexcept;
+// placement (3)	
+// void operator delete (void* ptr, void* voidptr2) noexcept;
+
+class A {
+public:
+    A() {
+        std::cout << "construct" << std::endl;
+    }
+    explicit A(int a):a(a) {
+        std::cout << "another construct" << std::endl;
+    }
+    ~A() {
+        std::cout << "deconstruct" << std::endl;
+    }
+
+    A(const A& other) {
+        std::cout << "copy construct" << std::endl;
+        this->a = other.a;
+    }
+
+    A& operator=(const A& other) {
+        std::cout << "copy assign construct" << std::endl;
+        this->a = other.a;
+        return *this;
+    }
+
+    A(A&& other) {
+        std::cout << "move construct" << std::endl;
+        this->a = other.a;
+    }
+
+    A& operator=(A&& other) {
+        std::cout << "move assign construct" << std::endl;
+        this->a = other.a;
+        return *this;
+    }
+
+    bool operator<(const A &a) const {
+      std::cout << "operator<, this " << this->a << ", other: " << a.a << std::endl;
+      return this->a < a.a;
+    }
+
+    friend std::ostream& operator<<(std::ostream &output, const A &a) { 
+        output << a.a;
+        return output;            
+    }
+
+    bool operator==(const A &a) const {
+      std::cout << "operator==, this " << this->a << ", other: " << a.a << std::endl;
+      return this->a == a.a;
+    }
+
+public:
+    int a = 0;
+};
+
+void* operator new(std::size_t size) {
+  std::cout << "operator new" << std::endl;
+  return malloc(size);
+}
+
+void* operator new(std::size_t size, const std::nothrow_t &nothrow_value) noexcept {
+  std::cout << "operator new nothrow" << std::endl;
+  return malloc(size);
+}
+
+void operator delete(void *ptr) noexcept {
+  std::cout << "operator delete" << std::endl;
+  free(ptr);
+}
+
+void operator delete(void *ptr, const std::nothrow_t &nothrow_constant) noexcept {
+  std::cout << "operator delete nothrow" << std::endl;
+  free(ptr);
+}
+
+// new handler
+void no_memory () {
+  std::cout << "Failed to allocate memory!\n";
+  // std::exit (1);
+}
+
+void NewTest() {
+  std::cout << "**********new handler**********" << std::endl;
+  std::set_new_handler(no_memory);
+  std::new_handler p = std::get_new_handler();
+  p();
+
+  std::cout << "**********operator new**********" << std::endl;
+  A *a = new A(10);
+  std::cout << "*a: " << *a << std::endl;
+
+  std::cout << "**********operator new nothrow**********" << std::endl;
+  A *b = new (std::nothrow) A(20);
+  if(b == nullptr) {
+    std::cout << "new failed" << std::endl;
+  } else {
+    std::cout << "*b:" << *b << std::endl;
+  }
+  
+  std::cout << "**********operator replace new**********" << std::endl;
+  A *c = new (b) A(30); // c跟a地址一样
+  std::cout << "*b:" << *b << std::endl;
+  std::cout << "*c:" << *c << std::endl;
+
+  std::cout << "**********malloc**********" << std::endl;
+  A *d = (A*)malloc(sizeof(A));
+  new (d) A(40);
+  std::cout << "*d:" << *d << std::endl;
+
+  std::cout << "**********operator new but no construct**********" << std::endl;
+  A *e = (A*) ::operator new(sizeof(A));
+  std::cout << "*e:" << *e << std::endl;
+
+  std::cout << "**********operator delete**********" << std::endl;
+  delete a;
+  delete b;
+  // delete c; // 会报错，因为c是b的地址，不能重复释放
+  free(d);
+  ::operator delete(e);
+
+  std::cout << "**********operator delete after**********" << std::endl;
+  // 依然可以使用，但是此时b已经被释放了，所以会有问题
+  std::cout << "*b: " << *b << std::endl;
+}
+
 int main() {
   person p1(10);
   person p2(10);
@@ -133,4 +272,7 @@ int main() {
 
   OutExample oe(10);
   std::cout << "OutExample: " << oe << std::endl;
+
+  std::cout << "=================================operator new===================================" << std::endl;
+  NewTest();
 }
