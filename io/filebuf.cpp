5cfa825e2c0262607b5c4476b70b762677011763
chenguangzeng <chenguangzeng@megvii.com>
2025-02-15 01:54:55 +0800

    feat: add io

--- io/filebuf.cpp
+++ io/filebuf.cpp
@@ -89,6 +89,85 @@ void MemFun() {
  */
 void PubMemFunInhertStreambuff() {
   // std::streambuf buf; // 编译报错is protected within this context
+  std::filebuf fb;
+  std::filebuf* ret = fb.open("/mnt/workspace/cgz_workspace/Exercise/exercises/io/test.txt", std::ios::in);
+  if(ret == nullptr){
+    std::cout << "open file failed" << std::endl;
+  } else {
+    std::cout << "open file success" << std::endl;
+  }
+  
+  // locale
+  std::locale loc = fb.pubimbue(std::locale(""));
+  std::cout << "before pubimbue: " << loc.name() << ", now " << std::locale("").name() << std::endl;
+  std::locale loc2 = fb.getloc();
+  std::cout << "getloc: " << loc2.name() << std::endl;
+
+  // buffer management
+  std::streambuf* buf = fb.pubsetbuf(nullptr, 0);
+  std::filebuf::pos_type pos = fb.pubseekoff(2, std::ios::beg, std::ios::in);
+  std::cout << "pubseekoff: " << pos << std::endl;
+  std::filebuf::pos_type pos_end = fb.pubseekoff(0, std::ios::end, std::ios::in);
+  std::cout << "pubseekoff end: " << pos_end << std::endl;
+  std::filebuf::pos_type pos2 = fb.pubseekpos(3, std::ios::in);
+  std::cout << "pubseekpos: " << pos2 << std::endl;
+  int sync = fb.pubsync();
+  if(sync == 0){
+    std::cout << "pubsync success" << std::endl;
+  } else {
+    std::cout << "pubsync failed" << std::endl;
+  }
+
+  // input
+  std::streamsize size = fb.in_avail();
+  std::cout << "in_avail: " << size << std::endl;
+  int get = fb.sgetc();
+  std::cout << "sgetc: " << get << std::endl;
+  int peek = fb.snextc();
+  std::cout << "snextc: " << peek << std::endl;
+  int peek2 = fb.sbumpc();
+  std::cout << "sbumpc: " << peek2 << std::endl;
+  char array[10];
+  array[9] = '\0';
+  std::streamsize getn = fb.sgetn(array, 8);
+  std::cout << "sgetn: " << getn << std::endl;
+  std::cout << "array: " << array << std::endl;
+  int put_back = fb.sputbackc('2'); // 回退到符合条件的字符位置，失败则按照具体实现
+  if(put_back == EOF){
+    std::cout << "sputbackc failed" << std::endl;
+  } else {
+    std::cout << "sputbackc: " << put_back << ", sgetc " << fb.sgetc() << std::endl;
+  }
+  std::cout << "getc: " << fb.sgetc() << std::endl;
+  int unget = fb.sungetc(); // 倒数第二个字符
+  if(unget == EOF){
+    std::cout << "sungetc failed" << std::endl;
+  } else {
+    std::cout << "sungetc: " << unget << ", sgetc " << fb.sgetc() << std::endl;
+  }
+  fb.close();
+
+  // put
+  std::filebuf fb2;
+  std::filebuf *ret2 = fb2.open("/mnt/workspace/cgz_workspace/Exercise/exercises/io/test.txt", std::ios::out | std::ios::app);
+  if(ret2 == nullptr){
+    std::cout << "open file2 failed" << std::endl;
+  } else {
+    std::cout << "open file2 success" << std::endl;
+  }
+  fb2.pubseekoff(2, std::ios::beg, std::ios::out | std::ios::in);
+  int put = fb2.sputc('b');
+  put = fb2.sputc('c');
+  if(put == EOF){
+    std::cout << "sputc failed" << std::endl;
+  } else {
+    std::cout << "sputc: " << put << std::endl;
+  }
+  char array2[10] = "123456789";
+  std::streamsize putn = fb2.sputn(array2, 8);
+  std::cout << "sputn: " << putn << std::endl;
+
+  fb2.close();
 }
 
 int main() {
--- io/locale.cpp
+++ io/locale.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
 #include <locale>
+#include <type_traits>
+#include <string>
+
+class MyFacet : public std::locale::facet {
+public:
+    MyFacet(std::size_t refs) : std::locale::facet(refs) {}
+    ~MyFacet() {}
+    static std::locale::id id;
+};
+
+void MemType() {
+    if(std::is_same<std::locale::category, int>::value) {
+        std::cout << "std::locale::category is int" << std::endl;
+    } else {
+        std::cout << "std::locale::category is not int" << std::endl;
+    }
+    if(std::is_same<std::locale::id, int>::value) {
+        std::cout << "std::locale::id is int" << std::endl;
+    } else {
+        std::cout << "std::locale::id is not int" << std::endl;
+    }
+    if(std::is_same<std::locale::facet, int>::value) {
+        std::cout << "std::locale::facet is int" << std::endl;
+    } else {
+        std::cout << "std::locale::facet is not int" << std::endl;
+    }
+}
+
+void Confun() {
+    std::locale loc = std::locale();
+    std::locale loc2(loc);
+    std::locale loc3("en_US.UTF8");
+    std::locale loc4 = std::locale(loc3, "", std::locale::ctype);
+
+    // std::locale::facet *f = new MyFacet(0);
+    // std::locale loc5(loc4, &f);
+
+    std::cout << "loc: constructor" << std::endl;
+}
+
+void MemFun() {
+    std::locale loc = std::locale::classic(); // 等价于 std::locale loc("C");
+
+    std::locale loc2("");
+    loc2 = loc2.combine<std::num_put<char>>(std::locale::classic());
+
+    std::locale loc3("C");
+    loc3.global(std::locale(""));
+    std::locale loc4("C");
+    std::cout << "loc3 == loc4: " << ((loc3 == loc4)?"True":"False") << std::endl;
+
+    std::locale loc5("");
+    std::string name = loc5.name();
+    std::cout << "loc5.name(): " << name << std::endl;
+
+    std::cout << "MemFun" << std::endl;
+}
 
 int main() {
-  
+    std::cout << "======================MemType======================" << std::endl;
+    MemType();
+    std::cout << "======================Confun======================" << std::endl;
+    Confun();
+    std::cout << "======================MemFun======================" << std::endl;
+    MemFun();
 }
