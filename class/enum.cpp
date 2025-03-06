428d2bac901ae2552b368e3a6dc6af234ea1c84c
chenguangzeng <chenguangzeng@megvii.com>
2025-03-06 20:34:33 +0800

    feat: add proto

---
+++ class/enum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+enum class RadarIndex {
+  FRONT4D,
+  FRONT_RIGHT,
+  FRONT_LEFT,
+  REAR_RIGHT,
+  REAR_LEFT,
+};
+
+enum class RosMsgType : std::uint8_t {
+  PERCEPTION_RESULT,
+  FRONT_RADAR,
+  FRONT_RIGHT_RADAR,
+  FRONT_LEFT_RADAR,
+  REAR_RIGHT_RADAR,
+  REAR_LEFT_RADAR,
+};
+
+int main() {
+  RadarIndex radar_index = RadarIndex::FRONT4D;
+
+  // // 直接输出枚举值会报错，因为枚举没有重载输出运算符
+  // std::cout << "RadarIndex: " << radar_index << std::endl;
+
+  int front_left_radar = static_cast<int>(RosMsgType::FRONT_LEFT_RADAR);
+  std::cout << "RosMsgType FRONT_LEFT_RADAR: " << front_left_radar << std::endl;
+
+  // 编译报错：error: cannot convert ‘RosMsgType’ to ‘int’ in initialization
+  // int ret = RosMsgType::FRONT_LEFT_RADAR;
+
+  return 0;
+}
