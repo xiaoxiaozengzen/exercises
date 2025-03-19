004cff8dfa6777a4ae5fc3771cd59751e3badb20
chenguangzeng <chenguangzeng@megvii.com>
2025-03-19 21:48:55 +0800

    feat: add upper

--- algorithm/upper_bound.cpp
+++ algorithm/upper_bound.cpp
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include <vector>
 
-int main() {
+void normal() {
   int myints[] = {10,20,30,30,20,10,10,20};
   std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
 
@@ -18,8 +18,54 @@ int main() {
   low=std::lower_bound (v.begin(), v.end(), 20); // 返回第一个不小于20的元素位置
   up= std::upper_bound (v.begin(), v.end(), 20); // 返回第一个大于20的元素位置
 
-  std::cout << "lower_bound at position " << (low- v.begin()) << '\n';
-  std::cout << "upper_bound at position " << (up - v.begin()) << '\n';
+  if(low == v.end()) {
+    std::cout << "lower_bound not found" << std::endl;
+  } else {
+    std::cout << "lower_bound at position " << (low- v.begin()) << ", value is " << *low << std::endl;
+  }
+  
+  if(up == v.end()) {
+    std::cout << "upper_bound not found" << std::endl;
+  } else {
+    std::cout << "upper_bound at position " << (up - v.begin()) << ", value is " << *up << std::endl;
+  }
+}
+
+void abnormal() {
+  int myints[] = {10,20,30,30,20,10,10,20};
+  std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
+
+  std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
+
+  std::vector<int>::iterator low, up, low2;
+  low=std::lower_bound (v.begin(), v.end(), 9); // 返回第一个不小于9的元素位置
+  low2=std::lower_bound (v.begin(), v.end(), 31); // 返回第一个不小于31的元素位置
+  up= std::upper_bound (v.begin(), v.end(), 31); // 返回第一个大于31的元素位置
+
+  if(low == v.end()) {
+    std::cout << "lower_bound not found" << std::endl;
+  } else {
+    std::cout << "lower_bound at position " << (low- v.begin()) << ", value is " << *low << std::endl;
+  }
+
+  if(low2 == v.end()) {
+    std::cout << "lower_bound not found" << std::endl;
+  } else {
+    std::cout << "lower_bound at position " << (low2- v.begin()) << ", value is " << *low2 << std::endl;
+  }
+  
+  if(up == v.end()) {
+    std::cout << "upper_bound not found" << std::endl;
+  } else {
+    std::cout << "upper_bound at position " << (up - v.begin()) << ", value is " << *up << std::endl;
+  }
+}
+
+int main() {
+  std::cout << "==================== normal ====================" << std::endl;
+  normal();
+  std::cout << "==================== abnormal ====================" << std::endl;
+  abnormal();
 
   return 0;
 }
