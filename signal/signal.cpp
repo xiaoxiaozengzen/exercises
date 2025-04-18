a50112b48d0cfd844f13e452aca46ed386c6c0ef
chenguangzeng <chenguangzeng@megvii.com>
2025-04-18 15:18:38 +0800

    feat: add signal

--- signal/signal.cpp
+++ signal/signal.cpp
@@ -9,12 +9,29 @@
 #include <iostream>
 
 /**
-  对于应用程序自行处理的信号来说，信号的生命周期要经过信号的安装登记、信号集操作、信号的发送和信号的处理四个阶段。
-　　1）信号的安装登记指的是在应用程序中，安装对此信号的处理方法。
-　　2）信号集操作的作用是用于对指定的一个或多个信号进行信号屏蔽。
-　　3）信号的发送指的是发送信号，可以通过硬件（如在终端上按下Ctrl-C）发送的信号和软件（如通过kill函数）发送的信号。
-　　4）信号的处理指的是操作系统对接收信号进程的处理，处理方法是先检查信号集操作函数是否对此信号进行屏蔽，如果没有屏蔽，操作系统将按信号安装函数中登记注册的处理函数完成对此进程的处理。
+ * 对于应用程序自行处理的信号来说，信号的生命周期要经过信号的安装登记、信号集操作、信号的发送和信号的处理四个阶段。
+ * 1）信号的安装登记指的是在应用程序中，安装对此信号的处理方法。
+ * 2）信号集操作的作用是用于对指定的一个或多个信号进行信号屏蔽。
+ * 3）信号的发送指的是发送信号，可以通过硬件（如在终端上按下Ctrl-C）发送的信号和软件（如通过kill函数）发送的信号。
+ * 4）信号的处理指的是操作系统对接收信号进程的处理，处理方法是先检查信号集操作函数是否对此信号进行屏蔽，如果没有屏蔽，操作系统将按信号安装函数中登记注册的处理函数完成对此进程的处理。
  *
+ */
+
+/**
+ * 实时信号和非实时信号的区别：
+ * 1.kill -l显示的信号列表中：1-31号信号为非实时信号(非可靠)，34-64号信号为实时信号(可靠)
+ * 
+ * 非实时信号缺点：
+ * 1.阻塞信号可能会丢失。当一个信号阻塞时，这个信号即使多次发送给进程，也被执行一次信号句柄。
+ * 2.信号交付没有携带与信号有关信息。接受到信号的进程无法区分同种信号的不同情况，也不知道信号从何而来。
+ * 3. 信号的交付没有优先级。当有多个信号悬挂与一个进程时，交付的顺序不确定
+ * 
+ * 实时信号相比非实时信号的优点：
+ * 1.实时信号和标准信号不一样，他没有明确的含义，而是由使用者自己来决定如何使用。
+ * 2.进程可以接受多个同样的实时信号，而标准信号不能，在标准信号没有得到处理的时候，多个标准信号会被合为一个。
+ * 3.实时信号使用sigqueue发送的时候，可以携带附加的数据(int或者pointer)
+ * 4.实时信号有时间顺序的概念，所以同样的实时信号会按次序被处理。
+ * 5.实时信号有优先级的概念，数值越小优先级越高，数值越大优先级越低。
  */
 
 /************************************1.1.信号安装*************************************************/
@@ -42,17 +59,45 @@ void signal_example() {
 // oldact: 指向用于保存旧的信号处理动作的指针。如果为 NULL，则不保存旧的信号处理动作。
 
 // struct sigaction {
-//     void (*sa_handler)(int);           // 信号处理函数指针
-//     void (*sa_sigaction)(int, siginfo_t *, void *); // 信号处理函数指针（扩展）
+//     void (*sa_handler)(int);           // 信号处理函数指针，一般用来处理非实时信号(当然也可以处理实时信号)
+//     void (*sa_sigaction)(int, siginfo_t *, void *); // 拓展信号处理函数指针，传递信号值以及其他信息，一般用来处理实时信号
 //     sigset_t sa_mask;                  // 在处理该信号时要阻塞的信号集
 //     int sa_flags;                      // 修改信号行为的选项
 //     void (*sa_restorer)(void);         // 恢复函数，已废弃
 // };
-// sa_mask字段说明了一个信号集，在调用该信号捕捉函数之前，这一信号集要加进进程的信号屏蔽字中。仅当从信号捕捉函数返回时再将进程的信号屏蔽字复位为原先值。
+// sa_mask字段说明了一个信号集，指定在信号处理程序执行过程中，哪些信号应当被阻塞。缺省情况下当前信号本身被阻塞，防止信号的嵌套发送，
 // sa_flags字段说明了信号处理的行为，列举了一些值，这些值可以通过or进行使用：
 //      #define	SA_NOCLDSTOP  1		 /* Don't send SIGCHLD when children stop.  */
 //      #define SA_NOCLDWAIT  2		 /* Don't create zombie on child death.  */
 //      #define SA_SIGINFO    4		 /* Invoke signal-catching function with
+
+// 不同平台siginfo_t结构体定义不同
+// siginfo_t {
+//   int      si_signo;     /* Signal number */
+//   int      si_errno;     /* An errno value */
+//   int      si_code;      /* Signal code */
+//   int      si_trapno;    /* Trap number that caused hardware-generated signal(unused on most architectures) */
+//   pid_t    si_pid;       /* Sending process ID */
+//   uid_t    si_uid;       /* Real user ID of sending process */
+//   int      si_status;    /* Exit value or signal */
+//   clock_t  si_utime;     /* User time consumed */
+//   clock_t  si_stime;     /* System time consumed */
+//   union sigval si_value; /* Signal value */
+//   int      si_int;       /* POSIX.1b signal */
+//   void    *si_ptr;       /* POSIX.1b signal */
+//   int      si_overrun;   /* Timer overrun count; POSIX.1b timers */
+//   int      si_timerid;   /* Timer ID; POSIX.1b timers */
+//   void    *si_addr;      /* Memory location which caused fault */
+//   long     si_band;      /* Band event (was int in glibc 2.3.2 and earlier) */
+//   int      si_fd;        /* File descriptor */
+//   short    si_addr_lsb;  /* Least significant bit of address (since Linux 2.6.32) */
+//   void    *si_lower;     /* Lower bound when address violation occurred (since Linux 3.19) */
+//   void    *si_upper;     /* Upper bound when address violation occurred (since Linux 3.19) */
+//   int      si_pkey;      /* Protection key on PTE that caused fault (since Linux 4.6) */
+//   void    *si_call_addr; /* Address of system call instruction (since Linux 3.5) */
+//   int      si_syscall;   /* Number of attempted system call (since Linux 3.5) */
+//   unsigned int si_arch;  /* Architecture of attempted system call (since Linux 3.5) */
+// } 
 void sigaction_example() {
   struct sigaction act;
   act.sa_handler = signal_handler;
@@ -208,6 +253,30 @@ void multi_thread() {
   t2.join();
 }
 
+void signal_habdler_info(int signo, siginfo_t *info, void *context) {
+  printf("getpid: %d\n", getpid());
+  printf("getid: %d\n", gettid());
+  printf("signal_habdler_info: %d\n", signo);
+  printf("signal_habdler_info.si_signo: %d\n", info->si_signo);
+  printf("signal_habdler_info.si_errno: %d\n", info->si_errno);
+  printf("signal_habdler_info.si_code: %d\n", info->si_code);
+  printf("signal_habdler_info.si_pid: %d\n", info->si_pid);
+  printf("signal_habdler_info.si_uid: %d\n", info->si_uid);
+  printf("signal_habdler_info.si_status: %d\n", info->si_status);
+  printf("signal_habdler_info.si_utime: %ld\n", info->si_utime);
+  printf("signal_habdler_info.si_stime: %ld\n", info->si_stime);
+}
+void SigInfoTest() {
+  sigset_t set;
+  sigemptyset(&set);
+  struct sigaction act;
+  act.sa_sigaction = signal_habdler_info;
+  act.sa_flags = 0;
+  sigaction(SIGABRT, &act, NULL);
+
+  sigqueue(getpid(), SIGABRT, {10});
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("Usage: %s <option>\n", argv[0]);
@@ -222,5 +291,7 @@ int main(int argc, char *argv[]) {
   sigset_example();
   std::cerr << "====================== system_call ======================" << std::endl;
   system_call(option);
+  std::cerr << "====================== SigInfoTest ======================" << std::endl;
+  SigInfoTest();
   std::this_thread::sleep_for(std::chrono::seconds(10));
 }
