332deb71d569db3db58a5aee2dded52103ce07d3
chenguangzeng <chenguangzeng@megvii.com>
2025-04-19 15:23:10 +0800

    feat: add time

--- time/chrono.cpp
+++ time/chrono.cpp
@@ -80,24 +80,42 @@ void duration_class() {
 
 /**
  * template <class Clock, class Duration = typename Clock::duration>  class time_point;
+ * 
+ * 1.表示相对于epoch的时间点，epoch是一个特定的时间点，通常是1970年1月1日00:00:00 UTC。
+ * 2.它需要依赖Clock类提供的time_point类型。clock类有system_clock, steady_clock和high_resolution_clock。
  */
 void timepoint_class() {
-  std::chrono::system_clock::time_point tp_epoch;  // epoch value
+  typedef std::chrono::time_point<std::chrono::system_clock> time_point_type;  // system_clock
+  // member type
+  if(std::is_same<time_point_type::clock, std::chrono::system_clock>::value) {
+    std::cout << "time_point_type::clock is std::chrono::system_clock" << std::endl;
+  }
+  if(std::is_same<time_point_type::duration, std::chrono::system_clock::duration>::value) {
+    std::cout << "time_point_type::duration is std::chrono::system_clock::duration" << std::endl;
+  }
+  if(std::is_same<time_point_type::rep, std::chrono::system_clock::rep>::value) {
+    std::cout << "time_point_type::rep is std::chrono::system_clock::rep" << std::endl;
+  }
+  if(std::is_same<time_point_type::period, std::chrono::system_clock::period>::value) {
+    std::cout << "time_point_type::period is std::chrono::system_clock::period" << std::endl;
+  }
 
-  std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<int>> tp_seconds(
-      std::chrono::duration<int>(1));
+  //construct functions
+  time_point_type tp;  // default constructor, epoch value
+  time_point_type tp_1(time_point_type::duration(1));  // from duration object
+  std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<int, std::milli>> tp_2(
+      std::chrono::duration<int, std::milli>(10));  // from duration object
+  time_point_type tp_3(tp_2);  // copy / from time_point
 
-  // copy or move
-  std::chrono::system_clock::time_point tp(tp_seconds);
-
-  std::cout << "1 second since system_clock epoch = ";
-  std::cout << tp.time_since_epoch().count();
-  std::cout << " system_clock periods." << std::endl;
+  // member functions
+  std::chrono::duration<int, std::milli> dtn_2 = tp_2.time_since_epoch();  // epoch value
+  time_point_type::duration dtn_1 = tp_1.time_since_epoch();  // epoch value
+  std::cout << "dtn_1: " << dtn_1.count() << std::endl;
+  std::cout << "dtn_2: " << dtn_2.count() << std::endl;
 
   // display time_point:
   std::time_t tt = std::chrono::system_clock::to_time_t(tp);
   std::cout << "time_point tp is: " << ctime(&tt);
-
   std::chrono::system_clock::time_point tp1, tp2;                          // epoch value
   std::chrono::system_clock::duration dtn(std::chrono::duration<int>(1));  // 1 second
 
@@ -116,31 +134,74 @@ void timepoint_class() {
   std::cout << "dtn: " << dtn.count() << std::endl;
 }
 
-void systemclock() {
-  std::chrono::duration<int, std::ratio<60 * 60 * 24>> one_day(1);
+/**
+ * class system_clock
+ * 1.realtime: 该类表示系统的实时时钟，通常用于获取当前的日期和时间。
+ * 2.time point: 该类的时间点是有符号的，意味着它可以表示epoch之前的时间。
+ * 3.system-wide: 使用该类获取时间点时，这个系统上所有进程调用其接口获取的时间点都是一致的。
+ */
+void system_clock_class() {
+  // member type
+  if(std::is_same<std::chrono::system_clock::rep, int>::value) {
+    std::cout << "std::chrono::system_clock::rep is int" << std::endl;
+  } else {
+    std::cout << "std::chrono::system_clock::rep is not int" << std::endl;
+  }
+  if(std::is_same<std::chrono::system_clock::period, std::ratio<1>>::value) {
+    std::cout << "std::chrono::system_clock::period is std::ratio<1>" << std::endl;
+  } else {
+    std::cout << "std::chrono::system_clock::period is not std::ratio<1>" << std::endl;
+  }
+  if(std::is_same<std::chrono::system_clock::duration, std::chrono::duration<int>>::value) {
+    std::cout << "std::chrono::system_clock::duration is std::chrono::duration<int>" << std::endl;
+  } else {
+    std::cout << "std::chrono::system_clock::duration is not std::chrono::duration<int>" << std::endl;
+  }
+  if(std::is_same<std::chrono::system_clock::time_point, std::chrono::time_point<std::chrono::system_clock>>::value) {
+    std::cout << "std::chrono::system_clock::time_point is std::chrono::time_point<std::chrono::system_clock>" << std::endl;
+  } else {
+    std::cout << "std::chrono::system_clock::time_point is not std::chrono::time_point<std::chrono::system_clock>" << std::endl;
+  }
 
-  std::chrono::system_clock::time_point today = std::chrono::system_clock::now();
-  std::chrono::system_clock::time_point tomorrow = today + one_day;
+  // member constants
+  std::cout << "std::chrono::system_clock::is_steady: " << std::chrono::system_clock::is_steady << std::endl;
 
-  std::cout << std::boolalpha << "is ready: " << std::chrono::system_clock::is_steady << std::endl;
+  // static member functions
+  std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();  // 获取当前时间点
+  time_t tt = std::chrono::system_clock::to_time_t(tp);  // 转换为time_t类型
+  std::cout << "current time: " << ctime(&tt);  // 转换为字符串格式输出
+  time_t tt2 = time(nullptr);  // 获取当前时间点
+  std::chrono::system_clock::time_point tp2 = std::chrono::system_clock::from_time_t(tt2);  // 转换为time_point类型
+}
 
-  std::cout << "today: " << today.time_since_epoch().count() << std::endl;
+/**
+ * class steady_clock
+ * 1.monotonic: 单调时间钟，表示时间是单调递增的，不会回退。
+ * 2.steday：时钟前进的每一个tick都是相同的，表示时间是稳定的。
+ */
+void steady_clock_class() {
+  // 同system_clock，但是静态成员函数只有now()
+}
 
-  std::time_t tt;
-
-  tt = std::chrono::system_clock::to_time_t(today);
-  std::cout << "today is: " << ctime(&tt);
-
-  tt = std::chrono::system_clock::to_time_t(tomorrow);
-  std::cout << "tomorrow will be: " << ctime(&tt);
+/**
+ * class high_resolution_clock
+ * 1.high resolution: 提供最高精度的时钟，它可能是steady_clock或system_clock的别名。
+ */
+void high_resolution_clock_class() {
+  // 同system_clock，但是静态成员函数只有now()
 }
 
 void time_function() {
+  /**
+   * template <class ToDuration, class Rep, class Period>
+   * constexpr ToDuration duration_cast (const duration<Rep,Period>& dtn);
+   * 
+   * @brief 将一个duration对象转换为另一个duration对象，其period可以不同。
+   */
   std::chrono::seconds s(1);  // 1 second
   std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(s);
 
   ms += std::chrono::milliseconds(2500);  // 2500 millisecond
-
   s = std::chrono::duration_cast<std::chrono::seconds>(ms);  // truncated
 
   std::cout << "ms: " << ms.count() << std::endl;
@@ -148,9 +209,14 @@ void time_function() {
 
   typedef std::chrono::duration<int, std::ratio<60 * 60 * 24>> days_type;
 
+  /**
+   * template <class ToDuration, class Clock, class Duration>
+   * time_point<Clock,ToDuration> time_point_cast (const time_point<Clock,Duration>& tp);
+   * 
+   * @brief 将一个time_point对象转换为另一个time_point对象，其duration可以不同。
+   */
   std::chrono::time_point<std::chrono::system_clock, days_type> today =
       std::chrono::time_point_cast<days_type>(std::chrono::system_clock::now());
-
   std::cout << today.time_since_epoch().count() << " days since epoch" << std::endl;
 }
 
@@ -181,8 +247,12 @@ int main() {
   duration_class();
   std::cout << "=======================timepoint_class=======================" << std::endl;
   timepoint_class();
-  std::cout << "=======================systemclock=======================" << std::endl;
-  systemclock();
+  std::cout << "=======================system_clock_class=======================" << std::endl;
+  system_clock_class();
+  std::cout << "=======================steady_clock_class=======================" << std::endl;
+  steady_clock_class();
+  std::cout << "=======================high_resolution_clock_class=======================" << std::endl;
+  high_resolution_clock_class();
   std::cout << "=======================time_function=======================" << std::endl;
   time_function();
   std::cout << "=======================chrono_literals=======================" << std::endl;
