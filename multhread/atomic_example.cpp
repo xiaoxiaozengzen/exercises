ac4559aaaa717da37971723764cf409c42e3f86e
chenguangzeng <chenguangzeng@megvii.com>
2025-04-20 00:03:56 +0800

    feat: add atomic

--- multhread/atomic_example.cpp
+++ multhread/atomic_example.cpp
@@ -10,32 +10,32 @@ https://cplusplus.com/reference/atomic/atomic/
 #include <thread>
 #include <vector>
 
+/**
+ * template <class T> struct atomic;
+ * 
+ * 拷贝构造函数和赋值运算符被删除
+ */
 void show_atomic_base() {
   std::atomic<int> defalut_value;
   std::atomic<int> value(10);
 
-  // Indicates whether the object is lock-free.
-  std::cout << "is_lock_free: " << defalut_value.is_lock_free() << std::endl;
+  /**************** general operation ****************** */
   std::cout << "is_lock_free: " << value.is_lock_free() << std::endl;
-
-  std::cout << "defalut_value: " << defalut_value << std::endl;
-
   value.store(20);
-  value.store(25);
-  std::cout << "value: " << value << std::endl;
-
   int loda_ret = value.load();
   std::cout << "loda_ret: " << loda_ret << std::endl;
-
   int ret = value;
   std::cout << "ret: " << ret << std::endl;
-
   int before_ret = value.exchange(30);
   std::cout << "before_ret: " << before_ret << ", value: " << value << std::endl;
 
-  // compare_exchange_weak(expected, value)
-    // if true, it replaces the contained value with value
-    // if false, it replaces expected with the contained value .
+  // Compares the contents of the atomic object's contained value with expected:
+  // if true, it replaces the contained value with value
+  // if false, it replaces expected with the contained value .
+
+  // 区别：
+  // compare_exchange_weak() 允许失败，即实际obtain值和expected相等，但是函数返回false
+  // compare_exchange_strong() 不允许失败，
   int expected_value = 30;
   int new_value = 35;
   std::cout << "value: " << value << std::endl;
@@ -50,7 +50,7 @@ void show_atomic_base() {
             << ", expected value: " << expected_value << ", new_value: " << new_value << std::endl;
 }
 
-void show_atomic_spe() {
+void show_atomic_specifaction() {
   std::atomic<int> value(10);
   value.fetch_add(10);
   std::cout << "value: " << value << std::endl;
@@ -84,9 +84,11 @@ void show_atomic_flag() {
   std::atomic_flag fl = ATOMIC_FLAG_INIT;
 
   auto append = [&](int id) {
+    // test_and_set()函数会将标志位设置为true，并返回原来的标志位
     while (fl.test_and_set()) {
     }
     std::cout << "id: " << id << std::endl;
+    // 清楚标志位，设置成false。使得下次调用set_and_test()返回false
     fl.clear();
   };
 
@@ -188,13 +190,42 @@ void TestSpinLock() {
   }
 }
 
+void FunForAtomicCStyle() {
+  std::atomic<int> value(10);
+  bool is_lock_free = atomic_is_lock_free(&value);
+  std::cout << "atomic_is_lock_free(&value): " << is_lock_free << std::endl;
+
+  // 对已经初始化过的原子对象调用atomic_init()是未定义行为
+  std::atomic<int> value2;
+  atomic_init(&value2, 20);
+  std::cout << "value2: " << value2.load() << std::endl;
+
+  atomic_store(&value2, 30);
+  std::cout << "value2: " << value2.load() << std::endl;
+
+  int ret = atomic_load(&value2);
+  std::cout << "atomic_load(&value2): " << ret << std::endl;
+
+  int before_ret = atomic_exchange(&value2, 40);
+  std::cout << "before_ret: " << before_ret << ", value2: " << value2.load() << std::endl;
+
+  std::atomic_flag flag = ATOMIC_FLAG_INIT;
+  bool is_set_before = atomic_flag_test_and_set(&flag);
+  std::cout << "is_set_before: " << is_set_before << std::endl;
+  atomic_flag_clear(&flag);
+
+}
+
 int main() {
   std::cout << "==================show_atomic_base====================" << std::endl;
   show_atomic_base();
-  std::cout << "==================show_atomic_spe====================" << std::endl;
-  show_atomic_spe();
+  std::cout << "==================show_atomic_specifaction====================" << std::endl;
+  show_atomic_specifaction();
   std::cout << "==================show_atomic_flag====================" << std::endl;
   show_atomic_flag();
   std::cout << "==================TestSpinLock====================" << std::endl;
   TestSpinLock();
+  std::cout << "==================FunForAtomicCStyle====================" << std::endl;
+  FunForAtomicCStyle();
+  return 0;
 }
