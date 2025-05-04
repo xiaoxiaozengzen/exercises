d207ab429b4004ec3adefc1cc9a98a1da79e1477
chenguangzeng <chenguangzeng@megvii.com>
2025-05-04 22:10:10 +0800

    feat: add algo

--- algorithm/upper_bound.cpp
+++ algorithm/upper_bound.cpp
@@ -4,6 +4,15 @@
 // template <class ForwardIterator, class T, class Compare>
 // ForwardIterator upper_bound (ForwardIterator first, ForwardIterator last, const T& val, Compare comp);
 
+/**
+ * Compare comp
+ * @note 接收两个参数，返回bool值
+ * @param first 第一个参数是const T& val中的val
+ * @param second 被迭代器指向的元素
+ * @return true: first < second
+ * @return false: first >= second
+ */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -61,11 +70,40 @@ void abnormal() {
   }
 }
 
+struct MyLess {
+  bool operator() (const int& first, const int& second) {
+    return (first < second);
+  }
+};
+
+void custom_compare() {
+  int myints[] = {10,20,30,30,20,10,10,20};
+  std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
+
+  std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
+
+  std::vector<int>::iterator low,up;
+  low=std::lower_bound (v.begin(), v.end(), 20, MyLess()); // 返回第一个不小于20的元素位置
+  up= std::upper_bound (v.begin(), v.end(), 20, MyLess()); // 返回第一个大于20的元素位置
+  if(low == v.end()) {
+    std::cout << "lower_bound not found" << std::endl;
+  } else {
+    std::cout << "lower_bound at position " << (low- v.begin()) << ", value is " << *low << std::endl;
+  }
+  if(up == v.end()) {
+    std::cout << "upper_bound not found" << std::endl;
+  } else {
+    std::cout << "upper_bound at position " << (up - v.begin()) << ", value is " << *up << std::endl;
+  }
+}
+
 int main() {
   std::cout << "==================== normal ====================" << std::endl;
   normal();
   std::cout << "==================== abnormal ====================" << std::endl;
   abnormal();
+  std::cout << "==================== custom_compare ====================" << std::endl;
+  custom_compare();
 
   return 0;
 }
