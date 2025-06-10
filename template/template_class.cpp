81a2a44755a0d2576fc4a8ae4d98fc2b696efdcc
chenguangzeng <chenguangzeng@megvii.com>
2025-06-10 14:06:05 +0800

    feat: add template

--- template/template_class.cpp
+++ template/template_class.cpp
@@ -85,7 +85,7 @@ void class_template_virtual_fun() {
 
 
 /******************************3. 模板类的特化******************************************** */
-template <typename T>
+template <typename T, typename U>
 class SpecializationClass {
  public:
   SpecializationClass() {
@@ -101,7 +101,7 @@ class SpecializationClass {
    * @note 同一个类模板实例化的不同的类，彼此之间毫无关系
    * @note 而静态数据成员是属于类的，而不是模板类；模板类实例化的不同的类，它们的静态数据成员不是同一个，请注意。
    */
-  static const int value = 0;
+  static const int value = 10;
 
 #if 0
   /**
@@ -115,13 +115,29 @@ class SpecializationClass {
     std::cout << "SpecializationClass::print : " << t << std::endl;
   }
 #endif
+
+  /**
+   * @note 可以类外定义
+   */
+  void print();
 };
+
+template <typename T, typename U>
+void SpecializationClass<T, U>::print() {
+  std::cout << "SpecializationClass::print" << std::endl;
+}
+
+/**
+ * @note 模板类的成员函数不能偏特化，参考函数模板
+ * @note 特化的成员函数必须类外实现 
+ * 
+ */
 
 /**
  * @brief 模板类的全特化
  */
 template <>
-class SpecializationClass<int> {
+class SpecializationClass<int, int> {
  public:
   SpecializationClass() {
     std::cout << "SpecializationClass<int>()" << std::endl;
@@ -135,11 +151,101 @@ class SpecializationClass<int> {
   }
 };
 
+/**
+ * @brief 模板类的部分特化
+ * 
+ * @note 部分特化是指只特化模板参数的一部分，而不是全部。
+ * @note 这里特化了第一个模板参数为double，第二个模板参数为任意类型U。
+ */
+template <typename U>
+class SpecializationClass<double, U> {
+ public:
+  SpecializationClass() {
+    std::cout << "SpecializationClass<double, U>()" << std::endl;
+  }
+  ~SpecializationClass() {
+    std::cout << "~SpecializationClass<double, U>()" << std::endl;
+  }
+  void print() {
+    std::cout << "SpecializationClass<double, U>::print" << std::endl;
+  }
+  void my_print();
+};
+
+template <typename U>
+void SpecializationClass<double, U>::my_print() {
+  std::cout << "SpecializationClass<double, U>::my_print" << std::endl;
+}
+
+
+void class_template_specialization() {
+  SpecializationClass<char, double> sc1;
+  std::cout << "SpecializationClass<char, double>::value: " << SpecializationClass<char, double>::value << std::endl;
+  sc1.print();
+
+  SpecializationClass<int, int> sc2;
+  sc2.print(2);
+
+  SpecializationClass<double, int> sc3;
+  sc3.print();
+  sc3.my_print();
+}
+
+/****************************************4. 成员函数模板*********************************************** */
+/**
+ * @brief 类模板中的成员函数模板
+ * 
+ * @note 成员函数模板可以在类模板中定义，也可以在类模板外定义。
+ * @note 成员函数模板可以是虚函数，但不能是纯虚函数。
+ */
+template <typename T>
+class TemplateClass {
+ public:
+  TemplateClass() {
+    std::cout << "TemplateClass()" << std::endl;
+  }
+  ~TemplateClass() {
+    std::cout << "~TemplateClass()" << std::endl;
+  }
+
+  template <typename U>
+  void print(U t) {
+    std::cout << "TemplateClass::print : " << t << std::endl;
+  }
+
+  /**
+   * @brief 不是模板函数，就是一个普通的成员函数
+   */
+  void my_print(T t) {
+    std::cout << "TemplateClass::print : " << t << std::endl;
+  }
+};
+
+/**
+ * @brief 普通类中的模板函数
+ */
+class TemplateFun {
+ public:
+  TemplateFun() {
+    std::cout << "TemplateFun()" << std::endl;
+  }
+  ~TemplateFun() {
+    std::cout << "~TemplateFun()" << std::endl;
+  }
+
+  template <typename T>
+  void print(T t) {
+    std::cout << "TemplateFun::print : " << t << std::endl;
+  }
+};
+
 
 int main() {
   std::cout << "===================== class_template_fun=====================" << std::endl;
   class_template_fun();
   std::cout << "===================== class_template_virtual_fun=====================" << std::endl;
   class_template_virtual_fun();
+  std::cout << "===================== class_template_specialization=====================" << std::endl;
+  class_template_specialization();
   return 0;
 }
