1e68c5f9dfa4e9ceddfceea1172adb4a9d49a24d
chenguangzeng <chenguangzeng@megvii.com>
2025-06-29 22:03:12 +0800

    feat: add time print

--- string/string.cpp
+++ string/string.cpp
@@ -63,7 +63,7 @@ void ConFun() {
     std::cout << "s10: " << s10 << ", size: " << s10.size() << std::endl;
 }
 
-void MemFun() {
+void iteratorTest() {
     // 1.iterator
     std::string s1("abcdefg");
     std::string::iterator it1 = s1.begin();
@@ -71,8 +71,11 @@ void MemFun() {
     std::string::reverse_iterator it3 = s1.rbegin();
     std::string::const_reverse_iterator it4 = s1.crbegin();
     std::cout << "it1: " << *it1 << ", it2: " << *it2 << ", it3: " << *it3 << ", it4: " << *it4 << std::endl;
+}
 
+void CapacityTest() {
     // 2.capacity
+    std::string s1("abcdefg");
     std::size_t size = s1.size();
     std::size_t length = s1.length();
     std::size_t max_size = s1.max_size();
@@ -82,6 +85,7 @@ void MemFun() {
     std::cout << "s1: " << s1 << ", size: " << s1.size() << ", length: " << s1.length() << ", capacity: " << s1.capacity() << std::endl;
     s1.resize(5, 'i');
     std::cout << "s1: " << s1 << ", size: " << s1.size() << ", length: " << s1.length() << ", capacity: " << s1.capacity() << std::endl;
+    // void reserve(std::size_t n = 0);
     s1.reserve(20);
     std::cout << "s1: " << s1 << ", size: " << s1.size() << ", length: " << s1.length() << ", capacity: " << s1.capacity() << std::endl;
     s1.reserve();
@@ -93,18 +97,24 @@ void MemFun() {
     }
     s1.resize(10, 'a');
     s1.shrink_to_fit();
-    std::cout << "s1: " << s1 << ", size: " << s1.size() << ", length: " << s1.length() << ", capacity: " << s1.capacity() << std::endl;
+    std::cout << "s1: " << s1 << ", size: " << s1.size() << ", length: " << s1.length() << ", capacity: " << s1.capacity() << std::endl;    
+}
 
+void elementAccessTest() {
     // 3.element access
+    std::string s1("abcdefg");
     char ch1 = s1[0];
     char ch2 = s1.at(1);
     s1.front() = 'b';
     char ch3 = s1.front();    
     char ch4 = s1.back();
     std::cout << "ch1: " << ch1 << ", ch2: " << ch2 << ", ch3: " << ch3 << ", ch4: " << ch4 << std::endl;
+}
 
+void ModifiersTest() {
     // 4.modifiers
     // 4.1 +=
+    std::string s1("abcdefgh");
     std::string s2("abcdefghdefgh");
     const char* cstr = "def";
     char ch5 = 'g';
@@ -189,15 +199,54 @@ void MemFun() {
     // 4.9 pop_back
     s1.pop_back();
     std::cout << "s1: " << s1 << std::endl;
-    
+}
+
+void OperationsTest() {
     // 5.string operations
+    // 5.1. c_str
+    std::string s1("abcdefg");
     const char* cstr1 = s1.c_str();
     std::cout << "cstr1: " << cstr1 << std::endl;
+    // 5.2. data
     const char* data = s1.data();
     std::cout << "data: " << data << std::endl;
     char arr[] = "12345";
+    // 5.3. copy: 当前字符串的子串拷贝到arr中。从pos=1开始拷贝len=5个字符到arr中。不会帮你生成'\0'结尾的字符串。
     std::size_t ret = s1.copy(arr, 5, 1);
     std::cout << "arr: " << arr << ", ret: " << ret << std::endl;
+    // 5.4. find
+    std::string another_string("cdefg");
+    char* another_char = "def";
+    std::size_t pos1 = s1.find(another_string, 0);
+    std::cout << "find another_string: " << another_string << ", pos1: " << pos1 << std::endl;
+    std::size_t pos2 = s1.find(another_char, 0);
+    std::cout << "find another_char: " << another_char << ", pos2: " << pos2 << std::endl;
+    std::size_t pos3 = s1.find(another_char, 0, 2);
+    std::cout << "find another_char with length: " << another_char << ", pos3: " << pos3 << std::endl;
+    std::size_t pos4 = s1.find('c', 0);
+    std::cout << "find char 'c': pos4: " << pos4 << std::endl;
+    // 5.5. find_first_of
+    std::size_t pos5 = s1.find_first_of(another_string, 0);
+    std::cout << "find_first_of another_string: " << another_string << ", pos5: " << pos5 << std::endl;
+    std::size_t pos6 = s1.find_first_of(another_char, 0);
+    std::cout << "find_first_of another_char: " << another_char << ", pos6: " << pos6 << std::endl;
+    std::size_t pos7 = s1.find_first_of(another_char, 0, 2);
+    std::cout << "find_first_of another_char with length: " << another_char << ", pos7: " << pos7 << std::endl;
+    std::size_t pos8 = s1.find_first_of('c', 0);
+    std::cout << "find_first_of char 'c': pos8: " << pos8 << std::endl;
+    // 5.6. substr: 当前字符串从pos=1开始，len=3的子串。
+    std::string sub_str1 = s1.substr(1, 3);
+    std::cout << "substr from pos=1, len=3: " << sub_str1 << std::endl;
+    // 5.7. compare: 比较两个字符串是否相等。
+    std::string s2("abcdefg");
+    int cmp1 = s1.compare(s2);
+    if(cmp1 == 0) {
+        std::cout << "s1 is equal to s2" << std::endl;
+    } else if(cmp1 < 0) {
+        std::cout << "s1 is less than s2" << std::endl;
+    } else {
+        std::cout << "s1 is greater than s2" << std::endl;
+    }
 }
 
 int main() {
@@ -205,7 +254,15 @@ int main() {
     MemType();
     std::cout << "======================ConFun======================" << std::endl;
     ConFun();
-    std::cout << "======================MemFun======================" << std::endl;
-    MemFun();
+    std::cout << "======================iteratorTest======================" << std::endl;
+    iteratorTest();
+    std::cout << "======================CapacityTest======================" << std::endl;
+    CapacityTest();
+    std::cout << "======================elementAccessTest======================" << std::endl;
+    elementAccessTest();
+    std::cout << "======================ModifiersTest======================" << std::endl;
+    ModifiersTest();
+    std::cout << "======================OperationsTest======================" << std::endl;
+    OperationsTest();
     return 0;
 }
