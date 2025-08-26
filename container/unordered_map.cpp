cb6cb81859e6b9c062ef053facbc6f9138bc9e07
chenguangzeng <chenguangzeng@megvii.com>
2025-08-26 16:21:56 +0800

    feat: add radar time

--- container/unordered_map.cpp
+++ container/unordered_map.cpp
@@ -106,44 +106,24 @@ void BasicSetMemberTypes() {
   }
   if (std::is_same<std::unordered_map<double, int>::iterator,
                    std::iterator<std::input_iterator_tag, std::pair<const double, int>>>::value) {
-    std::cout << "iterator is std::iterator<std::input_iterator_tag, std::pair<const double, int>>"
-              << std::endl;
+    std::cout << "iterator is std::iterator<std::input_iterator_tag, std::pair<const double, int>>" << std::endl;
   } else {
-    std::cout
-        << "iterator is not std::iterator<std::input_iterator_tag, std::pair<const double, int>>"
-        << std::endl;
+    std::cout << "iterator is not std::iterator<std::input_iterator_tag, std::pair<const double, int>>" << std::endl;
   }
-  if (std::is_same<
-          std::unordered_map<double, int>::const_iterator,
-          std::iterator<std::input_iterator_tag, const std::pair<const double, int>>>::value) {
-    std::cout << "const_iterator is std::iterator<std::input_iterator_tag, const std::pair<const "
-                 "double, int>>"
-              << std::endl;
+  if (std::is_same<std::unordered_map<double, int>::const_iterator, std::iterator<std::input_iterator_tag, const std::pair<const double, int>>>::value) {
+    std::cout << "const_iterator is std::iterator<std::input_iterator_tag, const std::pair<const double, int>>" << std::endl;
   } else {
-    std::cout << "const_iterator is not std::iterator<std::input_iterator_tag, const "
-                 "std::pair<const double, int>>"
-              << std::endl;
+    std::cout << "const_iterator is not std::iterator<std::input_iterator_tag, const std::pair<const double, int>>" << std::endl;
   }
-  if (std::is_same<std::unordered_map<double, int>::local_iterator,
-                   std::iterator<std::input_iterator_tag, std::pair<const double, int>>>::value) {
-    std::cout
-        << "local_iterator is std::iterator<std::input_iterator_tag, std::pair<const double, int>>"
-        << std::endl;
+  if (std::is_same<std::unordered_map<double, int>::local_iterator, std::iterator<std::input_iterator_tag, std::pair<const double, int>>>::value) {
+    std::cout << "local_iterator is std::iterator<std::input_iterator_tag, std::pair<const double, int>>" << std::endl;
   } else {
-    std::cout << "local_iterator is not std::iterator<std::input_iterator_tag, std::pair<const "
-                 "double, int>>"
-              << std::endl;
+    std::cout << "local_iterator is not std::iterator<std::input_iterator_tag, std::pair<const double, int>>" << std::endl;
   }
-  if (std::is_same<
-          std::unordered_map<double, int>::const_local_iterator,
-          std::iterator<std::input_iterator_tag, const std::pair<const double, int>>>::value) {
-    std::cout << "const_local_iterator is std::iterator<std::input_iterator_tag, const "
-                 "std::pair<const double, int>>"
-              << std::endl;
+  if (std::is_same<std::unordered_map<double, int>::const_local_iterator, std::iterator<std::input_iterator_tag, const std::pair<const double, int>>>::value) {
+    std::cout << "const_local_iterator is std::iterator<std::input_iterator_tag, const std::pair<const double, int>>" << std::endl;
   } else {
-    std::cout << "const_local_iterator is not std::iterator<std::input_iterator_tag, const "
-                 "std::pair<const double, int>>"
-              << std::endl;
+    std::cout << "const_local_iterator is not std::iterator<std::input_iterator_tag, const std::pair<const double, int>>" << std::endl;
   }
 }
 
@@ -151,11 +131,8 @@ void ConFun() {
   std::unordered_map<std::string, int> m0 = {{"a", 1}, {"b", 2}, {"c", 3}};
   std::unordered_map<std::string, int> m1;      // default
   std::unordered_map<std::string, int> m2(10);  // initial bucket count
-  std::unordered_map<std::string, int> m3(
-      10, std::hash<std::string>());  // initial bucket count and hash function
-  std::unordered_map<std::string, int> m4(
-      10, std::hash<std::string>(),
-      std::equal_to<std::string>());  // initial bucket count, hash function and key equal function
+  std::unordered_map<std::string, int> m3(10, std::hash<std::string>());  // initial bucket count and hash function
+  std::unordered_map<std::string, int> m4(10, std::hash<std::string>(), std::equal_to<std::string>());  // initial bucket count, hash function and key equal function
   std::unordered_map<std::string, int> m5(m0.begin(), m0.end());             // range
   std::unordered_map<std::string, int> m6(m0);                               // copy
   std::unordered_map<std::string, int> m7(std::move(m0));                    // move
@@ -181,8 +158,7 @@ void MemFun() {
   std::cout << "m[\"a\"]: " << ref << std::endl;
   std::unordered_map<std::string, int>::mapped_type& ref2 = m["d"];
   std::cout << "m[\"d\"]: " << ref2 << std::endl;
-  std::unordered_map<std::string, int>::mapped_type& ref3 =
-      m.at("b");  // throw out_of_range exception if not found
+  std::unordered_map<std::string, int>::mapped_type& ref3 = m.at("b");  // throw out_of_range exception if not found
   std::cout << "m.at(\"b\"): " << ref3 << std::endl;
 
   // 4. element lookup
@@ -200,33 +176,25 @@ void MemFun() {
   }
   std::unordered_map<std::string, int>::size_type count = m.count("a");
   std::cout << "count(\"a\"): " << count << std::endl;
-  std::pair<std::unordered_map<std::string, int>::iterator,
-            std::unordered_map<std::string, int>::iterator>
-      range = m.equal_range("a");
+  std::pair<std::unordered_map<std::string, int>::iterator, std::unordered_map<std::string, int>::iterator> range = m.equal_range("a");
   if (range.first != m.end()) {
-    std::cout << "equal_range(\"a\"): " << range.first->first << ", " << range.first->second
-              << std::endl;
+    std::cout << "equal_range(\"a\"): " << range.first->first << ", " << range.first->second << std::endl;
   } else {
     std::cout << "equal_range(\"a\") not found" << std::endl;
   }
   if (range.second != m.end()) {
-    std::cout << "equal_range(\"a\"): " << range.second->first << ", " << range.second->second
-              << std::endl;
+    std::cout << "equal_range(\"a\"): " << range.second->first << ", " << range.second->second << std::endl;
   } else {
     std::cout << "equal_range(\"a\") not found" << std::endl;
   }
-  std::pair<std::unordered_map<std::string, int>::iterator,
-            std::unordered_map<std::string, int>::iterator>
-      rane2 = m.equal_range("e");
+  std::pair<std::unordered_map<std::string, int>::iterator, std::unordered_map<std::string, int>::iterator> rane2 = m.equal_range("e");
   if (rane2.first != m.end()) {
-    std::cout << "equal_range(\"e\"): " << rane2.first->first << ", " << rane2.first->second
-              << std::endl;
+    std::cout << "equal_range(\"e\"): " << rane2.first->first << ", " << rane2.first->second << std::endl;
   } else {
     std::cout << "equal_range(\"e\") not found" << std::endl;
   }
   if (rane2.second != m.end()) {
-    std::cout << "equal_range(\"e\"): " << rane2.second->first << ", " << rane2.second->second
-              << std::endl;
+    std::cout << "equal_range(\"e\"): " << rane2.second->first << ", " << rane2.second->second << std::endl;
   } else {
     std::cout << "equal_range(\"e\") not found" << std::endl;
   }
@@ -235,19 +203,15 @@ void MemFun() {
   // 5.1 emplace
   std::pair<std::unordered_map<std::string, int>::iterator, bool> result = m.emplace("d", 4);
   if (result.second) {
-    std::cout << "emplace(\"d\", 4): " << result.first->first << ", " << result.first->second
-              << std::endl;
+    std::cout << "emplace(\"d\", 4): " << result.first->first << ", " << result.first->second << std::endl;
   } else {
-    std::cout << "emplace(\"d\", 4) failed, old value: " << result.first->first << ", "
-              << result.first->second << std::endl;
+    std::cout << "emplace(\"d\", 4) failed, old value: " << result.first->first << ", " << result.first->second << std::endl;
   }
   std::pair<std::unordered_map<std::string, int>::iterator, bool> result2 = m.emplace("e", 5);
   if (result2.second) {
-    std::cout << "emplace(\"e\", 5): " << result2.first->first << ", " << result2.first->second
-              << std::endl;
+    std::cout << "emplace(\"e\", 5): " << result2.first->first << ", " << result2.first->second << std::endl;
   } else {
-    std::cout << "emplace(\"e\", 5) failed, old value: " << result2.first->first << ", "
-              << result2.first->second << std::endl;
+    std::cout << "emplace(\"e\", 5) failed, old value: " << result2.first->first << ", " << result2.first->second << std::endl;
   }
   // 5.2 emplace_hint
   std::unordered_map<std::string, int>::iterator it4 = m.emplace_hint(m.begin(), "f", 6);
@@ -255,17 +219,13 @@ void MemFun() {
   std::unordered_map<std::string, int>::iterator it5 = m.emplace_hint(m.begin(), "f", 7);
   std::cout << "emplace_hint(\"f\", 7): " << it5->first << ", " << it5->second << std::endl;
   // 5.3 insert
-  std::pair<std::unordered_map<std::string, int>::iterator, bool> result3 =
-      m.insert(std::make_pair("g", 7));
+  std::pair<std::unordered_map<std::string, int>::iterator, bool> result3 = m.insert(std::make_pair("g", 7));
   if (result3.second) {
-    std::cout << "insert(\"g\", 7): " << result3.first->first << ", " << result3.first->second
-              << std::endl;
+    std::cout << "insert(\"g\", 7): " << result3.first->first << ", " << result3.first->second << std::endl;
   } else {
-    std::cout << "insert(\"g\", 7) failed, old value: " << result3.first->first << ", "
-              << result3.first->second << std::endl;
+    std::cout << "insert(\"g\", 7) failed, old value: " << result3.first->first << ", " << result3.first->second << std::endl;
   }
-  std::unordered_map<std::string, int>::iterator it6 =
-      m.insert<std::pair<std::string, int>>(m.begin(), std::make_pair("h", 8));
+  std::unordered_map<std::string, int>::iterator it6 = m.insert<std::pair<std::string, int>>(m.begin(), std::make_pair("h", 8));
   std::cout << "insert(\"h\", 8): " << it6->first << ", " << it6->second << std::endl;
   std::unordered_map<std::string, int>::iterator it7 = m.insert(m.begin(), std::make_pair("h", 9));
   std::cout << "insert(\"h\", 9): " << it7->first << ", " << it7->second << std::endl;
@@ -311,8 +271,7 @@ void MemFun() {
   float max_load_factor = m.max_load_factor();
   std::cout << "max_load_factor: " << max_load_factor << std::endl;
   m.max_load_factor(0.5);
-  std::cout << "load_factor: " << m.load_factor() << ", max_load_factor: " << m.max_load_factor()
-            << std::endl;
+  std::cout << "load_factor: " << m.load_factor() << ", max_load_factor: " << m.max_load_factor() << std::endl;
   m.rehash(10);
   std::cout << "rehash(10): " << m.bucket_count() << ", load_factor: " << m.load_factor()
             << ", max_load_factor: " << m.max_load_factor() << std::endl;
@@ -328,13 +287,10 @@ void MemFun() {
 }
 
 int main() {
-  std::cout << "--------------------------------BasicSetMemberTypes--------------------------------"
-            << std::endl;
+  std::cout << "--------------------------------BasicSetMemberTypes--------------------------------" << std::endl;
   BasicSetMemberTypes();
-  std::cout << "--------------------------------ConFun--------------------------------"
-            << std::endl;
+  std::cout << "--------------------------------ConFun--------------------------------" << std::endl;
   ConFun();
-  std::cout << "--------------------------------MemFun--------------------------------"
-            << std::endl;
+  std::cout << "--------------------------------MemFun--------------------------------" << std::endl;
   MemFun();
 }
