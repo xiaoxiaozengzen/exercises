347605983a4d6e6c8b958c227a2a1ffc7e30d261
chenguangzeng <chenguangzeng@cgz.com>
2026-01-26 10:49:20 +0800

    feat: add

--- class/struct.cpp
+++ class/struct.cpp
@@ -84,11 +84,101 @@ void struct_construct() {
 #endif
 }
 
+/**
+ * 结构体内存字节对齐方式：
+ * 1.重排字段：把大字段放前面，小字段放后面，减少内存填充字节
+ * 2.显示填充字段：在成员中加入char padding[?]，手动填充字节
+ * 3.attribute
+ *   __attribute__((packed))：GCC和Clang支持，告诉编译器按1字节对齐，不进行填充
+ *   __attribute__((aligned(n)))：GCC和Clang支持，告诉编译器按n字节对齐，进行填充
+ * 4.#pragma pack(n)：MSVC、GCC和Clang支持，告诉编译器按n字节对齐，进行填充
+ *   一般用#pragma pack(push)保存当前对齐方式，#pragma pack(pop)恢复之前对齐方式
+ * 5.alignas(n)：C++11标准，告诉编译器按n字节对齐，进行填充
+ *
+ * @note
+ * padding于pack的区别：
+ * - packed：改变成员布局，取消成员间的填充字节
+ * - padding：不改变成员布局，调整整个结构体的大小为指定对齐方式的整数倍，在结构体末尾添加填充字节
+ */
+#pragma pack(push)
+#pragma pack(1)
+template<typename T>
+struct Point3D {
+  T x;
+  T y;
+  T z;
+};
+#pragma pack(pop)
+
+template<typename T>
+struct Point3D_derive : public Point3D<T> {
+  T a;
+};
+
+template<typename T>
+struct Point3D_derive_1 : public Point3D_derive<T> {
+  float b;
+};
+
+template<typename T>
+struct __attribute__((packed)) Point3D_attr {
+  T x;
+  T y;
+  T z;
+};
+
+template<typename T>
+struct __attribute__((aligned(8))) Point3D_attr_aligned {
+  T x;
+  T y;
+  T z;
+};
+
+template<typename T>
+struct __attribute__((packed, aligned(16))) Point3D_attr_both {
+  T x;
+  T y;
+  T z;
+};
+
+template<typename T>
+struct alignas(8) Point3D_alignas {
+  T x;
+  T y;
+  T z;
+};
+
+void pack_test() {
+  std::cout << "sizeof(Point3D<char>) = " << sizeof(Point3D<char>) << std::endl;
+  std::cout << "sizeof(Point3D<int>) = " << sizeof(Point3D<int>) << std::endl;
+  std::cout << "sizeof(Point3D<double>) = " << sizeof(Point3D<double>) << std::endl;
+  std::cout << "sizeof(Point3D_attr<char>) = " << sizeof(Point3D_attr<char>) << std::endl;
+  std::cout << "sizeof(Point3D_attr<int>) = " << sizeof(Point3D_attr<int>) << std::endl;
+  std::cout << "sizeof(Point3D_attr<double>) = " << sizeof(Point3D_attr<double>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_aligned<char>) = " << sizeof(Point3D_attr_aligned<char>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_aligned<int>) = " << sizeof(Point3D_attr_aligned<int>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_aligned<double>) = " << sizeof(Point3D_attr_aligned<double>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_both<char>) = " << sizeof(Point3D_attr_both<char>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_both<int>) = " << sizeof(Point3D_attr_both<int>) << std::endl;
+  std::cout << "sizeof(Point3D_attr_both<double>) = " << sizeof(Point3D_attr_both<double>) << std::endl;
+  std::cout << "sizeof(Point3D_alignas<char>) = " << sizeof(Point3D_alignas<char>) << std::endl;
+  std::cout << "sizeof(Point3D_alignas<int>) = " << sizeof(Point3D_alignas<int>) << std::endl;
+  std::cout << "sizeof(Point3D_alignas<double>) = " << sizeof(Point3D_alignas<double>) << std::endl;
+  std::cout << "sizeof(Point3D_derive<char>) = " << sizeof(Point3D_derive<char>) << std::endl;
+  std::cout << "sizeof(Point3D_derive<int>) = " << sizeof(Point3D_derive<int>) << std::endl;
+  std::cout << "sizeof(Point3D_derive<double>) = " << sizeof(Point3D_derive<double>) << std::endl;
+  std::cout << "sizeof(Point3D_derive_1<char>) = " << sizeof(Point3D_derive_1<char>) << std::endl;
+  std::cout << "sizeof(Point3D_derive_1<int>) = " << sizeof(Point3D_derive_1<int>) << std::endl;
+  std::cout << "sizeof(Point3D_derive_1<double>) = " << sizeof(Point3D_derive_1<double>) << std::endl;
+}
+
 int main() {
   std::cout << "======================Init======================" << std::endl;
   Init();
   std::cout << "==================struct_construct==================" << std::endl;
   struct_construct();
+  std::cout << "======================pack_test======================" << std::endl;
+  pack_test();
 
   return 0;
 }
